5713ecf7a2e662794aa9be0b5558966479b3d3e0
Piyush Kelotra <piyushk08w4pcc@gmail.com>
2026-06-02 12:46:40 +0530

    string operations comitted

---
+++ 04_operations.js
@@ -0,0 +1,29 @@
+let val =3;
+let NegVal = -val;
+
+console.log(val); // 3
+console.log(NegVal); // -3
+
+// The unary minus operator (-) is used to negate a number. It changes the sign of the number from positive to negative or vice versa. In this case, it takes the value of val (which is 3) and negates it to produce -3.
+
+
+// strings can be concatenated using the + operator
+
+let str1 = "Hello, ";
+let str2 = "world!";
+let result = str1 + str2;
+
+console.log(result); // "Hello, world!"
+
+// The + operator can also be used to concatenate strings. In this example, it combines str1 and str2 to create the new string "Hello, world!".
+
+// If you use the + operator with a string and a number, the number will be converted to a string and concatenated.
+
+let num = 42;
+let combined = "The answer is: " + num;
+
+console.log(combined); // "The answer is: 42"
+
+// In this case, the number 42 is converted to the string "42" and concatenated with "The answer is: ", resulting in the final string "The answer is: 42".
+
+
